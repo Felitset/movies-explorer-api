018b7cc62b8949605d82092b0ca747ccaf385189
olga <memento1991@rambler.ru>
2023-01-21 18:51:16 +0800

    added controllers + movie schema

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,15 +1,83 @@
-// const Movie = require('../models/movie');
+const Movie = require('../models/movie');
 
-const getAllSavedMovies = (req, res) => {
-  res.status(200).json({ message: 'return all saved movies' });
+const BadRequestError = require('../errors/400-bad-request');
+const AccessError = require('../errors/403-forbidden');
+const NotFoundError = require('../errors/404-not-found');
+
+const getAllSavedMovies = (req, res, next) => Movie
+  .find({})
+  .then((movies) => {
+    res.json(movies);
+  })
+  .catch(next);
+
+const postMovie = async (req, res, next) => {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+  } = req.body;
+  const owner = req.user._id;
+  const movie = await Movie
+    .create({
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailerLink,
+      thumbnail,
+      movieId,
+      nameRU,
+      nameEN,
+      owner,
+    })
+    .catch((err) => {
+      if (err.name === 'ValidatonError') {
+        next(new BadRequestError('Error DB validation'));
+      } else {
+        next(err);
+      }
+    });
+  return Movie.findById(movie._id)
+    .then((newmovie) => {
+      res.json(newmovie);
+    })
+    .catch(next);
 };
 
-const postMovie = (req, res) => {
-  res.status(200).json({ message: 'movie posted' });
-};
-
-const deleteMovie = (req, res) => {
-  res.status(200).json({ message: 'movie deleted' });
+const deleteMovie = (req, res, next) => {
+  const { movieId } = req.params;
+  Movie
+    .findById(movieId)
+    .then((movie) => {
+      if (!movie) {
+        throw new NotFoundError('Карточка не существует');
+      }
+      if (movie.owner.toString() !== req.user._id) {
+        throw new AccessError('Невозможно удалить не свою карточку');
+      }
+      return movie.deleteOne();
+    })
+    .then(() => {
+      res.status(200).json({ message: 'Card deleted successfuly' });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('WrongData'));
+      } else {
+        next(err);
+      }
+    });
 };
 
 module.exports = {
--- models/movie.js
+++ models/movie.js
@@ -2,18 +2,37 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const movieSchema = new mongoose.Schema({
-  likes: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    default: [],
-  }],
-  name: {
+  country: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
   },
-  link: {
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: [validator.isURL, 'Некорректная ссылка на аватар'],
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: [validator.isURL, 'Некорректная ссылка на аватар'],
+  },
+  thumbnail: {
     type: String,
     required: true,
     validate: [validator.isURL, 'Некорректная ссылка на аватар'],
@@ -23,12 +42,21 @@ const movieSchema = new mongoose.Schema({
     ref: 'user',
     required: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
+  movieId: {
+    // movie id from responce from MovieExplorer server
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
   },
 });
 
-const movie = mongoose.model('card', movieSchema);
+const movie = mongoose.model('movie', movieSchema);
 
 module.exports = movie;
